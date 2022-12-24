f57a7f06d261ccb7b4890ad1a87d122c461e4ef7
anton.bashirov <anton.sh.local@gmail.com>
2022-12-24 22:12:48 +0200

    fixes

---
+++ binding/dart/native/extension.h
@@ -0,0 +1,16 @@
+#ifndef BINDING_EXTENSION_H_INCLUDED
+#define BINDING_EXTENSION_H_INCLUDED
+
+#include "module.h"
+#include "msgpuck.h"
+
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
