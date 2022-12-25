c96e5a50430f6caed60f3a141741f14edffc1ebf
anton.bashirov <anton.sh.local@gmail.com>
2022-12-25 19:01:51 +0200

    fixes

--- binding/dart/lib/storage/executor.dart
+++ binding/dart/lib/storage/executor.dart
@@ -24,7 +24,7 @@ class StorageExecutor {
     _nativePort = _receiverPort.sendPort.nativePort;
   }
 
-  Future<void> transactional(FutureOr<T> Function<T>(StorageExecutor executor) function) => _begin()
+  Future<void> transactional(void Function<T>(StorageExecutor executor) function) => _begin()
       .then((_) {
         _transactional = true;
         function(this);
