2a44f1d017f0c871dcedcbfc83c84cadcf2e278c
anton.bashirov <anton.sh.local@gmail.com>
2022-12-26 14:44:42 +0200

    fixes

--- binding/dart/lib/storage/storage.dart
+++ binding/dart/lib/storage/storage.dart
@@ -20,7 +20,7 @@ class Storage {
   late int _ownerId;
   late RawReceivePort _shutdownPort;
 
-  Storage({String? libraryPath, void Function()? onShutdown}) {
+  Storage({String? libraryPath}) {
     _library = libraryPath != null
         ? File(libraryPath).existsSync()
             ? DynamicLibrary.open(libraryPath)
@@ -29,10 +29,7 @@ class Storage {
     _bindings = TarantoolBindings(_library);
     _ownerId = _bindings.tarantool_generate_owner_id();
     _defaultExecutor = executor();
-    _shutdownPort = RawReceivePort(() {
-      onShutdown?.call();
-      close();
-    });
+    _shutdownPort = RawReceivePort(() => close());
   }
 
   void boot(BootstrapScript script, MessageLoopConfiguration loopConfiguration) {
