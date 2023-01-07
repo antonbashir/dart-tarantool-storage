92e3a4f56600efa136991e3cf40e52d7b126158b
anton.bashirov <anton.sh.local@gmail.com>
2023-01-07 12:39:35 +0200

    search functinos

--- binding/dart/lua/search.lua
+++ binding/dart/lua/search.lua
@@ -1,58 +1,60 @@
 search = function(request)
-  return box.atomic(function()
-      local offset = request.offset
-      local limit = request.limit
-      local object = request.object
-      local key = request.key
-      local predicate = request.predicate
-      local mapper = request.mapper
-      local provider = request.provider
+    local offset = request.offset
+    local limit = request.limit
+    local object = request.object
+    local key = request.key
+    local predicate = request.predicate
+    local mapper = request.mapper
+    local provider = request.provider
 
-      local entities = {}
+    local entities = {}
 
-      local count = 0
-      local index = 0
+    local count = 0
+    local index = 0
 
-      if provider ~= nil then
-          local found = {}
-          for _, entity in object:pairs(key) do
-              if index >= offset then
-                  if count >= limit then
-                      break
-                  end
-                  if not found[entity.id:str()] then
-                      entity = provider(entity[1])
-                      if (predicate == nil or predicate(entity)) then
-                          if mapper ~= nil then
-                              entity = mapper(entity)
-                          end
-                          table.insert(entities, entity)
-                          found[entity[1]:str()] = true
-                          count = count + 1
-                      end
-                  end
-              end
-              index = index + 1
-          end
-          return entities
-      end
+    if provider ~= nil then
+        local found = {}
+        for _, entity in object:pairs(key) do
+            if index >= offset then
+                if count >= limit then
+                    break
+                end
+                if not found[entity.id:str()] then
+                    entity = provider(entity[1])
+                    if (predicate == nil or predicate(entity)) then
+                        if mapper ~= nil then
+                            entity = mapper(entity)
+                        end
+                        table.insert(entities, entity)
+                        found[entity[1]:str()] = true
+                        count = count + 1
+                    end
+                end
+            end
+            index = index + 1
+        end
+        return entities
+    end
 
-      for _, entity in object:pairs(key) do
-          if index >= offset then
-              if count >= limit then
-                  break
-              end
-              if predicate == nil or predicate(entity) then
-                  if mapper ~= nil then
-                      entity = mapper(entity)
-                  end
-                  table.insert(entities, entity)
-                  count = count + 1
-              end
-          end
-          index = index + 1
-      end
+    for _, entity in object:pairs(key) do
+        if index >= offset then
+            if count >= limit then
+                break
+            end
+            if predicate == nil or predicate(entity) then
+                if mapper ~= nil then
+                    entity = mapper(entity)
+                end
+                table.insert(entities, entity)
+                count = count + 1
+            end
+        end
+        index = index + 1
+    end
 
-      return entities
-  end)
+    return entities
+end
+
+searchAtomic = function(request)
+  return box.atomic(function() search(request) end)
 end
