b425c1a094746b732be27d708b2f92883207ed27
anton.bashirov <anton.sh.local@gmail.com>
2023-01-07 02:32:20 +0200

    fix script

--- build-static.sh
+++ build-static.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+make clean
+rm src/trivia/config.h || true
 cd static-build && make clean && cmake -DCMAKE_TARANTOOL_ARGS="-DCMAKE_BUILD_TYPE=RelWithDebInfo;-DENABLE_LTO=true" . && make -j
